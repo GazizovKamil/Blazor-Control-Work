688c461411f7dadd9110c3733cbe6eec69315759
Камиль Газизов <kamilgazizov09@gmail.com>
2022-10-28 23:26:37 +0300

    save image to DB

---
+++ Blazor Control Work/Data/FileSystemService.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+
+namespace Blazor_Control_Work.Data
+{
+    public class FileSystemService
+    {
+        public void UploadImageToDb(string namePhoto, string name)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Images321");
+            var gridFS = new GridFSBucket(database);
+
+            using (FileStream fs = new FileStream("C:\\Users\\Камиль\\Desktop\\image\\"+ namePhoto, FileMode.Open))
+            {
+                gridFS.UploadFromStream($"{name}.jpg", fs);
+            }
+        }
+
+        public void DownloadToLocal(string name)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Images321");
+            var gridFS = new GridFSBucket(database);
+            using (FileStream fs = new FileStream($"{Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Images/")}{name}+{"New"}.jpg", FileMode.CreateNew))
+            {
+                gridFS.DownloadToStreamByName($"{name}.jpg", fs);
+            }
+        }
+    }
+}
