18dafd52f7591080ba62785a96ecfbcaebefafed
Камиль Газизов <kamilgazizov09@gmail.com>
2023-02-08 22:44:34 +0300

    аа

--- Blazor Control Work/Program.cs
+++ Blazor Control Work/Program.cs
@@ -1,5 +1,6 @@
 using Blazor_Control_Work.Data;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,11 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<FileSystemService>();
 builder.Services.AddSingleton<SingletonService>();
-
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +24,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseResponseCompression();
 
 app.UseHttpsRedirection();
 
@@ -27,6 +33,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapHub<ChatHub>("/chat");
 app.MapFallbackToPage("/_Host");
 
 app.Run();
